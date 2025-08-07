924325f032a3b57ad22559c7c358200a4ecc0ac0
coded <coded@gmail.com>
2025-08-07 19:48:24 +0000

    updated

--- setup/settings.py
+++ setup/settings.py
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'setup.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,11 +118,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_URL = '/media/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'), 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- shop/models/products.py
+++ shop/models/products.py
@@ -14,9 +14,12 @@ class Product(BaseModel):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
     status = models.PositiveIntegerField()
     image = models.ImageField(blank=True)
+    stock_quantity = models.PositiveIntegerField(default=0)
+
+    actual_price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    selling_price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
 
     def __str__(self):
-        return f"Name: {self.name} Price: {self.price}"
+        return f"Name: {self.name} Price: {self.selling_price}"

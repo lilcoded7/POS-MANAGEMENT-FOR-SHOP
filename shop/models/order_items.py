0c11494d19a5eda3024fc6e3bc5b4804622136f4
coded <coded@gmail.com>
2025-08-22 16:01:28 +0000

    updated

--- shop/models/order_items.py
+++ shop/models/order_items.py
@@ -4,8 +4,9 @@ from shop.models.orders import Order
 from shop.models.products import Product
 from decimal import Decimal
 
+
 class OrderItem(BaseModel):
-    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="items")
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
     quantity = models.PositiveIntegerField(default=1)
 
@@ -15,4 +16,4 @@ class OrderItem(BaseModel):
     def get_total_product_price(self):
         if self.product and self.product.selling_price:
             return self.quantity * self.product.selling_price
-        return Decimal('0.00')
+        return Decimal("0.00")
